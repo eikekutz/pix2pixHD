4a19bb97b988cf39ce5ad77b6c90db547dd1ac75
ekutz <s182902@student.dtu.dk>
2020-07-15 16:45:06 +0200

    reduce training to 300 epochs

--- gland_train.sh
+++ gland_train.sh
@@ -36,6 +36,6 @@ module load python3/3.7.5
 
 /appl/cuda/10.2/samples/NVIDIA_CUDA-10.2_Samples/bin/x86_64/linux/release/deviceQuery
 
-python3 train.py --name gland  --dataroot=/zhome/95/c/135723/Datasets/Warwick_QU_Dataset_HD --no_instance --nThreads 1 --label_nc=0 --resize_or_crop resize_and_crop --gpu_ids 0 --loadSize=512 --fineSize=256 --niter=200 --niter_decay=200 --pool_size=200
+python3 train.py --name gland  --dataroot=/zhome/95/c/135723/Datasets/Warwick_QU_Dataset_HD --no_instance --nThreads 1 --label_nc=0 --resize_or_crop resize_and_crop --gpu_ids 0 --loadSize=512 --fineSize=256 --niter=150 --niter_decay=150 --pool_size=200
 
 
